d11ebab5dbfedd0af3e2132c6811f7490a04fb1f
Guilherme <guilherme.1009@hotmail.com>
2025-07-28 00:42:05 -0300

    feat: home page accessible only to logged in users

--- platform_app/views.py
+++ platform_app/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Imovel
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/auth/logar/')
 def home(request):
-    return HttpResponse("Welcome to the Home Page")
+    imoveis = Imovel.objects.all()
+    return render(request, 'home/home.html', {'imoveis': imoveis})
