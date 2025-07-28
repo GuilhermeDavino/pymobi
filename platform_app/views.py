123379d362c5554ec8b9daa0981973089cc8f288
Guilherme <guilherme.1009@hotmail.com>
2025-07-28 04:28:10 -0300

    feat: home, imovel and agendamentos pages completed

--- platform_app/views.py
+++ platform_app/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Imovel, Cidade
+from django.shortcuts import redirect, render
+from .models import Imovel, Cidade, Visitas
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 
@@ -36,3 +36,29 @@ def imovel(request, id):
     imovel = get_object_or_404(Imovel, id=id)
     sugestoes = Imovel.objects.filter(cidade=imovel.cidade).exclude(id=id)[:2]
     return render(request, 'imovel/imovel.html', {'imovel': imovel, 'sugestoes': sugestoes})
+
+
+def agendar_visitas(request):
+    usuario = request.user
+    dia = request.POST.get('dia')
+    horario = request.POST.get('horario')
+    id_imovel = request.POST.get('id_imovel')
+    visitas = Visitas(
+        imovel_id = id_imovel,
+        usuario = usuario,
+        dia = dia,
+        horario = horario
+    )
+    visitas.save()
+    return redirect('/agendamentos')
+
+def agendamentos(request):
+    visitas = Visitas.objects.filter(usuario=request.user)
+    return render(request, 'agendamentos/agendamentos.html', {'visitas': visitas})
+
+
+def cancelar_agendamento(request, id):
+    visitas = get_object_or_404(Visitas, id=id)
+    visitas.status = "C"
+    visitas.save()
+    return redirect('/agendamentos')
