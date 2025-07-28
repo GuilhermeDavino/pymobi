552a5d72185082571cb523093485db981ff58a65
Guilherme <guilherme.1009@hotmail.com>
2025-07-28 01:11:49 -0300

    feat: stylized home page and filtering properties by city, price and type, etc.

--- platform_app/views.py
+++ platform_app/views.py
@@ -1,9 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Imovel
+from .models import Imovel, Cidade
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/auth/logar/')
 def home(request):
-    imoveis = Imovel.objects.all()
-    return render(request, 'home/home.html', {'imoveis': imoveis})
+    preco_minimo = request.GET.get('preco_minimo')
+    preco_maximo = request.GET.get('preco_maximo')
+    cidade = request.GET.get('cidade')
+    tipo = request.GET.getlist('tipo')
+    
+    if preco_maximo or preco_minimo or cidade or tipo:
+        
+        if not preco_minimo:
+            preco_minimo = 0
+        
+        if not preco_maximo:
+            preco_maximo = 9999999999
+        
+        if not tipo:
+            tipo = ['A', 'C']
+
+        imoveis = Imovel.objects.filter(valor__gte=preco_minimo).filter(valor__lte=preco_maximo).filter(tipo_imovel__in=tipo).filter(cidade=cidade)
+    else:
+        imoveis = Imovel.objects.all()
+    
+    cidades = Cidade.objects.all()
+    return render(request, 'home/home.html', {'imoveis': imoveis, 'cidades': cidades})
